f2dbaaf71633c09df4bddab89ed54fb7d43d71e6
Erdem <erdemcanaz@gmail.com>
2024-07-20 23:00:13 +0300

    Server and Client -> get user allowed_tos

--- client/send_request.py
+++ client/send_request.py
@@ -15,11 +15,19 @@ def get_dummy_data(url:str = None, acces_token:str = None):
 
 
 if __name__ == '__main__':
+
     GET_TOKEN_ENDPOINT = f"{PARAM_SERVER_URL}/get_token"
-    acces_token, status_code = get_acces_token(url = GET_TOKEN_ENDPOINT, username="erdem.canaz", password="erdem123")
-
-
     GET_ALLOWED_TOS_ENDPOINT = f"{PARAM_SERVER_URL}/get_allowed_tos"
-    allowed_tos_data, status_code = get_dummy_data(url = GET_ALLOWED_TOS_ENDPOINT, acces_token=acces_token)
+    user_datas = [["erdem.canaz", "erdem123"], ["akif.tufan.perciner", "akif123"], ["ui.node", "ui123"], ["not_existing","123"]]
 
-    print(allowed_tos_data)
+    for user_data in user_datas:
+        print("\n")
+        username, password = user_data[0], user_data[1]    
+        print(f"Current user: {username}")    
+        print(f"Getting an acces token from {GET_TOKEN_ENDPOINT} with {username}'s credentials")
+        acces_token, status_code = get_acces_token(url = GET_TOKEN_ENDPOINT, username=username, password= password)
+        print(f"Getting privilages of the user by sending request to {GET_ALLOWED_TOS_ENDPOINT}")
+        allowed_tos_data, status_code = get_dummy_data(url = GET_ALLOWED_TOS_ENDPOINT, acces_token=acces_token)
+        print(f"{username} is allowed to -> {allowed_tos_data}")
+
+
