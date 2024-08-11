dca02bb61f33b155d6082591ecd0fd5171fa3206
Erdem <erdemcanaz@gmail.com>
2024-08-11 13:49:54 +0300

    currently at password or username indicator

--- client/pages/login_page.py
+++ client/pages/login_page.py
@@ -9,9 +9,9 @@ class LoginPage():
 
     CONSTANTS = {
         "allowed_keys": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{}|;':,.<>/?`~ ",
-        "username_bbox": (222, 481, 800, 550),
+        "username_bbox": (222, 481, 800, 547),
         "username_text_bleft": (300, 527),
-        "password_bbox": (222, 605, 800,670),
+        "password_bbox": (222, 605, 800,667),
         "password_text_bleft": (300, 650),
         "show_password_bbox": (814, 605, 883, 670),
 
@@ -77,17 +77,51 @@ class LoginPage():
             if self.secondary_mode == 1 and len(active_user.get_username())>0: #username
                 active_user.set_username(new_username = active_user.get_username()[:-1])
             elif self.secondary_mode == 2 and len(active_user.get_password())>0:
-                active_user.set_password(new_password = active_user.get_password()[:-1])                
+                active_user.set_password(new_password = active_user.get_password()[:-1])    
+        elif pressed_key == 9: #TAB
+            if self.secondary_mode == 1:
+                self.secondary_mode = 2
+            elif self.secondary_mode == 2:
+                self.secondary_mode = 1  
         elif chr(pressed_key) in self.CONSTANTS["allowed_keys"]:
             if self.secondary_mode == 1:
                 active_user.set_username(new_username = active_user.get_username() + chr(pressed_key))
             elif self.secondary_mode == 2:
-                active_user.set_password(new_password = active_user.get_password() + chr(pressed_key))      
+                active_user.set_password(new_password = active_user.get_password() + chr(pressed_key))    
+        elif pressed_key == 10: #ENTER
+            status_code = None
+            try:
+                is_authenticated, status_code = active_user.get_acces_token()
+                print(f"User {active_user.get_username()} is authenticated -> {is_authenticated} -> {status_code}")
+            except Exception as e:
+                print(f"Error: {e}")       
+            
+            print(f"Status code: {status_code}")
+            if status_code is None: # server is not reachable
+                program_state[0] = 2
+                program_state[1] = 0
+                program_state[2] = 0    
+            elif status_code != 200: # user not found
+                active_user.set_username(new_username = "")
+                active_user.set_password(new_password = "")   
+            
+                program_state[0] = 3
+                program_state[1] = 0
+                program_state[2] = 0   
+            elif status_code == 200: # user authenticated, direct to which app page
+                program_state[0] = 4
+                program_state[1] = 0
+                program_state[2] = 0         
 
         # Draw UI
         picasso.draw_image_on_frame(ui_frame, image_name="login_page_template", x=0, y=0, width=1920, height=1080, maintain_aspect_ratio=True)  
         cv2.putText(ui_frame, active_user.get_username(), (self.CONSTANTS["username_text_bleft"][0], self.CONSTANTS["username_text_bleft"][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (170,95,0), 2, cv2.LINE_AA)
         
+        if self.secondary_mode == 1:
+            cv2.line(ui_frame, (self.CONSTANTS["username_bbox"][0]-10, self.CONSTANTS["username_bbox"][1]), (self.CONSTANTS["username_bbox"][0]-10, self.CONSTANTS["username_bbox"][3]), (170, 95, 0), 9)
+        elif self.secondary_mode == 2:
+            cv2.line(ui_frame, (self.CONSTANTS["password_bbox"][0]-10, self.CONSTANTS["password_bbox"][1]), (self.CONSTANTS["password_bbox"][0]-10, self.CONSTANTS["password_bbox"][3]), (170, 95, 0), 9)
+
         if self.show_password:
             cv2.putText(ui_frame, active_user.get_password(), (self.CONSTANTS["password_text_bleft"][0], self.CONSTANTS["password_text_bleft"][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (170,95,0), 2, cv2.LINE_AA)
             picasso.draw_image_on_frame(ui_frame, image_name="anil_right_looking", x=self.CONSTANTS["show_password_bbox"][0], y=self.CONSTANTS["show_password_bbox"][1], width=100, height=100, maintain_aspect_ratio=True)
