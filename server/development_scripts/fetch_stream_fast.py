d9cf8d1dd1a3a3651984bc8555b0a19d81920f44
Erdem <erdemcanaz@gmail.com>
2024-08-14 15:40:03 +0300

    Update fetch_stream_fast.py

--- server/development_scripts/fetch_stream_fast.py
+++ server/development_scripts/fetch_stream_fast.py
@@ -2,21 +2,22 @@ import cv2, time
 
 def fetch_single_frame_hundred_times(username:str = None, password:str = None, camera_ip_address:str = None):
    
+    start_time = time.time()
+    url = f'rtsp://{username}:{password}@{camera_ip_address}/{"profile2/media.smp"}'
+    cap = cv2.VideoCapture(url)
+    buffer_size_in_frames = 1
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size_in_frames)
+    init_time = time.time()
+    print(f"video stream opened in {init_time-start_time} seconds")
 
-    for i in range(100):
-        
-        start_time = time.time()
-        url = f'rtsp://{username}:{password}@{camera_ip_address}/{"profile2/media.smp"}'
-        cap = cv2.VideoCapture(url)
-        buffer_size_in_frames = 1
-        cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size_in_frames)
-        init_time = time.time()
+    while True:
+        start_time = time.time()      
         ret, frame = cap.read()
         if ret:
             cv2.imshow('frame', frame)
-            print(f"video stream opened in {init_time-start_time} seconds")
-            print(f"Frame {i} fetched in {time.time() - init_time} seconds")
-            wait_key = cv2.waitKey(10000)
+            print(f"Frame fetched in {time.time() - start_time} seconds")
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cap.release()
 
