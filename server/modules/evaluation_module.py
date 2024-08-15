0e227cb5ddd95681e7c8aeeb5a115f936e5956fc
Erdem <erdemcanaz@gmail.com>
2024-08-15 13:33:05 +0300

    Update evaluation_module.py

--- server/modules/evaluation_module.py
+++ server/modules/evaluation_module.py
@@ -38,6 +38,7 @@ class EvaluationManager():
                 self.recenty_evaluated_frame_uuids_wrt_camera[frame_info["camera_uuid"]] = {}
             elif frame_info["frame_uuid"] == self.recenty_evaluated_frame_uuids_wrt_camera[frame_info["camera_uuid"]]:
                 continue 
+            print(f"Frame is evaluated: {frame_info['camera_uuid']}, {frame_info['frame_uuid']}")
             self.recenty_evaluated_frame_uuids_wrt_camera[frame_info["camera_uuid"]] = frame_info["frame_uuid"]
 
             # Evaluate the frame based on the active rules
