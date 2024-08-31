81c82a07db3fc98339ad8ccb3a36db9c06f73656
Erdem <erdemcanaz@gmail.com>
2024-08-31 18:31:05 +0300

    Update main_fast_api_server.py

--- api_server/main_fast_api_server.py
+++ api_server/main_fast_api_server.py
@@ -222,7 +222,7 @@ async def create_reported_violation_api(report_violation_data: ReportViolation):
     report_violation_data_dict = report_violation_data.dict()
     report_violation_data_dict.update({"save_folder": PREFERENCES.ENCRYPTED_IMAGE_FOLDER})
     try:
-        return {"reported_violation":  database_manager.create_reported_violation_v2(**report_violation_data.dict())}
+        return {"reported_violation":  database_manager.create_reported_violation_v2(**report_violation_data_dict)}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
