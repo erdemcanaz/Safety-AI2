b9c60d7a33cea695059f371e82998a9bbd7980f7
Erdem <erdemcanaz@gmail.com>
2024-08-31 17:28:29 +0300

    Update safety_ai_main.py

--- safety_ai/safety_ai_main.py
+++ safety_ai/safety_ai_main.py
@@ -1,5 +1,5 @@
 # Built-in imports
-import pprint, time, sys, os, cv2
+import pprint, time, sys, os, cv2, datetime
 from pathlib import Path
 
 # Local imports
@@ -26,7 +26,9 @@ camera_uuid = r[2]['camera_info'][0]['camera_uuid']
 r = api_dealer.update_count(camera_uuid=camera_uuid, count_type="TRIAL", delta_count=1)
 pprint.pprint(r)
 
-r = api_dealer.update_shift_count(camera_uuid=camera_uuid, shift_count=1)
+#self, camera_uuid:str=None, shift_date_ddmmyyyy:str=None, shift_no:str=None, count_type:str = None, delta_count:int = None
+shift_date = datetime.datetime.now().strftime("%d.%m.%Y")
+r = api_dealer.update_shift_count(camera_uuid=camera_uuid, shift_date_ddmmyyyy=shift_date, shift_no="0", count_type="TRIAL", delta_count=1)
 pprint.pprint(r)
 
 # Initialize the SQL database
