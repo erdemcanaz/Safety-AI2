49d4a02aabe78e36f2bf9cf7c9e113dba3bac374
Erdem <erdemcanaz@gmail.com>
2024-08-31 18:15:12 +0300

    .

--- PREFERENCES.py
+++ PREFERENCES.py
@@ -7,7 +7,7 @@ SAFETY_AI_USER_INFO = {"username": "safety_ai", "password": "safety_ai_password"
 if os.name == "nt":  # For Windows (i.e development environment)
     SERVER_IP_ADDRESS = "192.168.0.26"
     CLEAR_TERMINAL_COMMAND = "cls"
-    SQL_DATABASE_PATH = PREFERENCES_FILE_PATH/ "api_server" / "safety_ai.db"
+    SQL_DATABASE_PATH = PREFERENCES_FILE_PATH.parent/ "api_server" / "safety_ai.db"
     PRINT_MOUSE_COORDINATES = True
     ENCRYPTED_IMAGE_FOLDER = PREFERENCES_FILE_PATH.parent.parent / "safety_AI_volume" / "api_server" / "encrypted_images"
     ENCRYPTED_IMAGE_FOLDER_SSD = None
