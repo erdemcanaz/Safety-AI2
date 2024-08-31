f5d1d9810966800e353cc45b5cdecff82ba35cf5
Erdem <erdemcanaz@gmail.com>
2024-08-31 16:08:28 +0300

    Update update_cameras_page.py

--- client_ui/pages/update_cameras_page.py
+++ client_ui/pages/update_cameras_page.py
@@ -38,7 +38,7 @@ class UpdateCamerasPage:
         )
 
         self.camera_info = self.api_dealer.fetch_all_camera_info() # [True, 200, {'camera_info': [{'date_created': '2024-08-26 08:44:56', 'date_updated': '2024-08-26 08:44:56', 'camera_uuid': '92051d28-3402-46cd-952b-d2ba2782443e', 'camera_ip_address': '1.1.1.1', 'camera_region': "", 'camera_description': "", 'NVR_ip_address': '', 'stream_path': '/asdaf', 'camera_status': 'active'}, ...]]
-        self.cameras = [ {"COLUMN_0": camera['camera_ip_address'] if camera['camera_region'] != "" else camera['camera_region'], "camera_info":camera} for camera in self.camera_info[2]['camera_info']]
+        self.cameras = [ {"COLUMN_0": camera['camera_ip_address'] if camera['camera_region'] == "" else camera['camera_region'], "camera_info":camera} for camera in self.camera_info[2]['camera_info']]
         self.cameras_list_item.set_list_items(items = self.cameras)
 
         self.previous_page_button = ui_items.Button(
@@ -258,6 +258,7 @@ class UpdateCamerasPage:
         
         self.camera_info = self.api_dealer.fetch_all_camera_info() # [True, 200, {'camera_info': [{'username':'sdsa', 'password':'sdfsf', date_created': '2024-08-26 08:44:56', 'date_updated': '2024-08-26 08:44:56', 'camera_uuid': '92051d28-3402-46cd-952b-d2ba2782443e', 'camera_ip_address': '1.1.1.1', 'camera_region': "", 'camera_description': "", 'NVR_ip_address': '', 'stream_path': '/asdaf', 'camera_status': 'active'}, ...]]
         self.cameras = [ {"COLUMN_0": camera['camera_ip_address'] if camera['camera_region'] == "" else camera['camera_region'], "camera_info":camera} for camera in self.camera_info[2]['camera_info']]
+        pprint.pprint(self.cameras)
         self.cameras_list_item.set_list_items(items = self.cameras)
 
         # draw last frame 
