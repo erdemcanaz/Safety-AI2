fc716df34f14508d615026a9c843142c39ce4364
Erdem <erdemcanaz@gmail.com>
2024-10-01 11:00:06 +0300

    Update iot_device_ping.py

--- tests/development_scripts/iot_device_ping.py
+++ tests/development_scripts/iot_device_ping.py
@@ -1,5 +1,6 @@
 import serial.tools.list_ports
 import serial
+import time
 
 # List all available COM ports
 ports = serial.tools.list_ports.comports()
@@ -13,6 +14,10 @@ comport = ports[which_comport].device
 
 # Open the serial port
 ser = serial.Serial(comport, 9600)  # Make sure the baud rate matches your device
+
+# Wait for the device to be ready, since it resets when the serial connection is opened
+print("Waiting for the device to be ready for 5 seconds...")
+time.sleep(5)
 
 # Send '001780' as ASCII encoded data
 data_to_send = '001780'
