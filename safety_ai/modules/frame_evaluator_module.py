88db4eb5891724f47abceb5320e1060312304ab8
Erdem <erdemcanaz@gmail.com>
2024-10-01 19:29:19 +0300

    Update frame_evaluator_module.py

--- safety_ai/modules/frame_evaluator_module.py
+++ safety_ai/modules/frame_evaluator_module.py
@@ -197,9 +197,8 @@ class FrameEvaluator():
             is_right_ankle_in_restricted_area = self.__is_normalized_point_inside_polygon(right_ankle[:2], rule_polygon) if right_ankle[2] > 0 else False # negative confidence means that the keypoint is not detected
         
             if is_left_ankle_in_restricted_area or is_right_ankle_in_restricted_area and not is_person_inside_forklift:
-                print(f"Violation detected for rule_uuid: {rule_info['rule_uuid']}, violation_score: {violation_score}")
-               
                 violation_score = detection["bbox_confidence"] * (is_left_ankle_in_restricted_area * left_ankle[2] + is_right_ankle_in_restricted_area* right_ankle[2])/(is_left_ankle_in_restricted_area + is_right_ankle_in_restricted_area)
+                print(f"Violation detected for rule_uuid: {rule_info['rule_uuid']}, violation_score: {violation_score}")
                 evaluation_result['violation_person_nbboxes'].append(
                     {
                         "bbox": detection['bbox'],
@@ -258,9 +257,8 @@ class FrameEvaluator():
             
             is_person_in_restricted_area = self.__is_normalized_point_inside_polygon(normalized_bbox_center, rule_polygon)
             if is_person_in_restricted_area:
-                print(f"Violation detected for rule_uuid: {rule_info['rule_uuid']} violation_score: {violation_score}")
-
                 violation_score = detection["bbox_confidence"]
+                print(f"Violation detected for rule_uuid: {rule_info['rule_uuid']} violation_score: {violation_score}")
                 evaluation_result['violation_person_nbboxes'].append(
                     {
                         "bbox": detection['bbox'],
@@ -408,150 +406,6 @@ class FrameEvaluator():
                 else:
                     raise Exception(f"Unknown bbox_class_name: {closest_hardhat_detection['bbox_class_name']}")
             
-            evaluation_result['flags']['is_violation_detected'] = True
-            self.__draw_rect_on_frame(normalized_bbox= normalized_bbox, frame= processed_cv2_frame, color=[0, 0, 255], thickness=8)
-            # put HARDHAT icon on the top right (outside) corner of the bbox
-            bbox = self.__translate_normalized_bbox_to_frame_bbox(normalized_bbox, processed_cv2_frame)
-            icon_max_size = (bbox[3]-bbox[1])//3
-            padding = icon_max_size // 3
-            picasso_module.draw_image_on_frame(frame= processed_cv2_frame, image_name="red_hardhat_transparent_with_background", x = bbox[2] + padding , y = bbox[1], width=icon_max_size, height=icon_max_size, maintain_aspect_ratio=True)
-           
-            if violation_report_info['violation_score'] is None or violation_score > violation_report_info['violation_score']:
-                violation_report_info['violation_score'] = violation_score
-
-        if violation_report_info['violation_score'] is not None:
-            evaluation_result['violation_results'].append(violation_report_info)
-
-    def __check_person_isg_v1(self, evaluation_result:Dict, rule_info:Dict):
-        # ===============================================================================================
-        # evaluation type: hardhat_violation ||| evaluation method: v1
-        # If people head center is inside the defined area and the hardhat is not detected, then it is a violation.
-        # head center is the mean of the head keypoints (left_eye, right_eye, nose, left_ear, right_ear)
-        #
-        # Since hardhat is a small object the image is zoomed . the bbox of the person is checked if it fits inside
-        # 320x320 image. If so, a zoom frame that centers bbox is placed (320x320).
-        # then the image is resized to 640x640 and fed to the hardhat detection model.
-        # otherwise, the original frame is fed to the hardhat detection model.
-        #
-        # VIOLATION SCORE:
-        # -> if the hardhat is detected, then violation_score = 1 - (pose_bbox_confidence * hardhat_bbox_confidence)
-        # -> if the hardhat is not detected, then violation_score = pose_bbox_confidence
-        # Exception: If the person is inside the forklift, then it is not a violation.
-        # Dataset candidate: If a person is detected yet neiter hardhat or no_hardhat is detected, then it is a candidate for the dataset.
-        #================================================================================================
-        violation_report_info= { # Will not be added to the evaluation_result if no violation is detected
-            "camera_uuid": evaluation_result['frame_info']['camera_uuid'],
-            "region_name": evaluation_result['frame_info']['region_name'],
-            "violation_frame": None, # Will be added after the person blur is applied at the end of the evaluation, all rules share the same frame
-            "violation_date_ddmmyyy_hhmmss": datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
-            "violation_type": rule_info['rule_type'],
-            "violation_score": None, # will be added if a violation is detected           
-        }            
-        
-        frame_info = evaluation_result['frame_info']
-        processed_cv2_frame = evaluation_result['processed_cv2_frame']
-        rule_polygon = rule_info['rule_polygon']
-
-        # Ensure that the pose detection results are available
-        if evaluation_result['pose_detection_results'] is None: raise Exception("Pose detection results are not available for the hardhat violation evaluation")
-
-        # Get the forklift bboxes so that we can exclude them from the violation detection
-        evaluation_result['forklift_detection_results'] = self.forklift_detector.detect_frame(frame_info, bbox_threshold_confidence= PREFERENCES.FORKLIFT_MODEL_BBOX_THRESHOLD_CONFIDENCE)
-        normalized_forklift_bboxes = [detection['normalized_bbox'] for detection in evaluation_result['forklift_detection_results']['detections']]
-
-        for detection in evaluation_result['pose_detection_results']['detections']:
-            normalized_bbox = detection['normalized_bbox']
-            normalized_keypoints = detection['normalized_keypoints']
-
-            # Check if the person is inside the forklift, if so, continue
-            is_person_inside_forklift = False
-            for normalized_forklift_bbox in normalized_forklift_bboxes:
-                if self.__is_inside_another_bbox(normalized_bbox, normalized_forklift_bbox, intersection_percentage_threshold = 0.5):
-                    is_person_inside_forklift = True
-                    break
-            if is_person_inside_forklift: continue
-
-            # Calculate the head center of the person
-            xyn_total = [0, 0, 0]
-            for keypoint_name in ["left_eye", "right_eye", "nose", "left_ear", "right_ear"]:  
-                keypoint_info = normalized_keypoints[keypoint_name]
-                if keypoint_info[2] < 0: continue # Confidence is negative, i.e., the keypoint is not detected
-                xyn_total[0] += keypoint_info[0]
-                xyn_total[1] += keypoint_info[1]
-                xyn_total[2] += 1
-            if xyn_total[2] == 0: continue # No head keypoints are detected
-            normalized_head_center = [xyn_total[0]/xyn_total[2], xyn_total[1]/xyn_total[2]]
-
-            # Check if the head center of the person is inside the rule area
-            is_person_head_center_in_restricted_area = self.__is_normalized_point_inside_polygon(normalized_head_center, rule_polygon)
-            if not is_person_head_center_in_restricted_area: continue
-
-            # Zoom the bbox of the person if it fits inside the 320x320 image
-            frame_to_detect_hardhat = None
-
-            x1, y1, x2, y2 = self.__translate_normalized_bbox_to_frame_bbox(normalized_bbox, frame_info['cv2_frame'])
-            
-            # Calculate the width and height of the bbox
-            bbox_width = x2 - x1
-            bbox_height = y2 - y1
-
-            can_fit_inside_320x320 =  x2 - x1 <= 320 and y2 - y1 <= 320
-            if can_fit_inside_320x320:
-                # Calculate the center of the bbox
-                center_x = x1 + bbox_width // 2
-                center_y = y1 + bbox_height // 2
-
-                # Calculate the top-left corner of the 320x320 frame
-                start_x = max(center_x - 160, 0)  # Ensure x doesn't go below 0
-                start_y = max(center_y - 160, 0)  # Ensure y doesn't go below 0
-
-                # Adjust the bottom-right corner if 320x320 goes out of the original frame bounds
-                end_x = min(start_x + 320, frame_info['cv2_frame'].shape[1])
-                end_y = min(start_y + 320, frame_info['cv2_frame'].shape[0])
-
-                # If the adjustment causes the frame to be less than 320x320, adjust start_x and start_y
-                start_x = end_x - 320 if end_x - start_x < 320 else start_x
-                start_y = end_y - 320 if end_y - start_y < 320 else start_y
-
-                # Extract the 320x320 frame
-                zoom_frame = frame_info['cv2_frame'][start_y:end_y, start_x:end_x]
-                zoom_frame = cv2.resize(zoom_frame, (320, 320))  # Resize to 320x320 if necessary
-
-                # Resize the 320x320 frame to 640x640 if needed for further processing
-                resized_frame = cv2.resize(zoom_frame, (640, 640))
-                frame_to_detect_hardhat = resized_frame
-            else:
-                # If the bbox can't fit into a 320x320 frame, use the original frame
-                frame_to_detect_hardhat = frame_info['cv2_frame']
-
-            #cv2.imshow("hardhat_v1_zoomed_frame", frame_to_detect_hardhat) #TODO: make this parametric
-
-            evaluation_result['hardhat_detection_results'] = self.hardhat_detector.detect_frame(frame = frame_to_detect_hardhat, frame_info = None, bbox_threshold_confidence = PREFERENCES.HARDHAT_MODEL_BBOX_THRESHOLD_CONFIDENCE)
-            if len(evaluation_result['hardhat_detection_results']['detections']) == 0:
-                violation_score = detection["bbox_confidence"]
-                print(f"Violation detected for rule_uuid: {rule_info['rule_uuid']} violation_score: {violation_score} (model detects neither hard_hat nor no_hard_hat detection)")
-            else:
-                # find the closest hardhat bbox to the head center
-                min_distance = float("inf")
-                closest_hardhat_detection = None
-                for hardhat_detection in evaluation_result['hardhat_detection_results']['detections']:
-                    normalized_hardhat_bbox = hardhat_detection['normalized_bbox']
-                    normalized_hardhat_bbox_center = [(normalized_hardhat_bbox[0]+normalized_hardhat_bbox[2])/2, (normalized_hardhat_bbox[1]+normalized_hardhat_bbox[3])/2]
-                    normalized_distance = np.linalg.norm(np.array(normalized_head_center) - np.array(normalized_hardhat_bbox_center))
-                    if normalized_distance < min_distance:
-                        min_distance = normalized_distance
-                        closest_hardhat_detection = hardhat_detection
-
-                if closest_hardhat_detection['bbox_class_name'] == 'no_hard_hat':
-                    violation_score = detection["bbox_confidence"]
-                    print(f"Violation detected for rule_uuid: {rule_info['rule_uuid']} violation_score: {violation_score} (no_hardhat bbox)")
-                elif closest_hardhat_detection['bbox_class_name'] == 'hard_hat':
-                    violation_score = 1 - (detection["bbox_confidence"] * closest_hardhat_detection["bbox_confidence"])
-                    print(f"Violation detected for rule_uuid: {rule_info['rule_uuid']} violation_score: {violation_score} (hardhat bbox)")
-                else:
-                    raise Exception(f"Unknown bbox_class_name: {closest_hardhat_detection['bbox_class_name']}")
-            
-            evaluation_result['flags']['is_violation_detected'] = True
             self.__draw_rect_on_frame(normalized_bbox= normalized_bbox, frame= processed_cv2_frame, color=[0, 0, 255], thickness=8)
             # put HARDHAT icon on the top right (outside) corner of the bbox
             bbox = self.__translate_normalized_bbox_to_frame_bbox(normalized_bbox, processed_cv2_frame)
