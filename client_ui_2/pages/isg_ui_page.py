be0336ce1e64fb1616c66ff10701147352aa7016
Erdem <erdemcanaz@gmail.com>
2024-10-01 23:55:04 +0300

    Update isg_ui_page.py

--- client_ui_2/pages/isg_ui_page.py
+++ client_ui_2/pages/isg_ui_page.py
@@ -190,14 +190,14 @@ class ISG_UIpage:
                             evaluated_frame_count = count_dict['evaluated_frame_count']
                             picasso.draw_text_on_frame(
                                 self.page_frame, 
-                                text=f"Evaluated Frame Count: {evaluated_frame_count}", 
-                                position=(int(main_frame_location[0] * self.page_frame.shape[1]), int(main_frame_location[1] * self.page_frame.shape[0] + main_frame_height + 45)),
+                                text=f"{evaluated_frame_count:.0f}", 
+                                position=(int(main_frame_location[0] * self.page_frame.shape[1]+int(main_frame_width/2)), int(main_frame_location[1] * self.page_frame.shape[0] + main_frame_height + 70)),
                                 area_size=(int(main_frame_width/2), int(20)),
                                 alignment='center',
                                 font=cv2.FONT_HERSHEY_SIMPLEX, 
-                                font_scale = 0.5, 
-                                text_color=(0, 0, 0), 
-                                thickness=1, 
+                                font_scale=2,
+                                text_color=(169, 69, 0),
+                                thickness=2,
                                 padding=10
                             )
 
