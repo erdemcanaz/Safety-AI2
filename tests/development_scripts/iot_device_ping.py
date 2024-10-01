73ba828faad1cfd835bcf8d22458295054840d7c
Erdem <erdemcanaz@gmail.com>
2024-10-01 10:57:28 +0300

    Update iot_device_ping.py

--- tests/development_scripts/iot_device_ping.py
+++ tests/development_scripts/iot_device_ping.py
@@ -1,5 +1,16 @@
 #List all the comports available
 import serial.tools.list_ports
 ports = serial.tools.list_ports.comports()
-for port in ports:
-    print(port.device)
+for index, port in enumerate(ports):
+    print(index, port.device)
+
+which_comport = int(input("Enter the index of the comport you want to ping: "))
+comport = ports[which_comport].device
+
+import serial
+ser = serial.Serial(comport, 9600)
+ser.write(b'001780')
+print(ser.readline())
+
+
+
