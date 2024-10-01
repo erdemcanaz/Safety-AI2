74bcd697ac90fb8e8cde2a70e7b97737098f2fda
Erdem <erdemcanaz@gmail.com>
2024-10-01 10:58:55 +0300

    Update iot_device_ping.py

--- tests/development_scripts/iot_device_ping.py
+++ tests/development_scripts/iot_device_ping.py
@@ -1,16 +1,24 @@
-#List all the comports available
 import serial.tools.list_ports
+import serial
+
+# List all available COM ports
 ports = serial.tools.list_ports.comports()
 for index, port in enumerate(ports):
-    print(index, port.device)
+    print(f"{index}: {port.device}")
 
-which_comport = int(input("Enter the index of the comport you want to ping: "))
+# Ask the user to select the correct COM port
+which_comport = int(input("Enter the index of the COM port you want to use: "))
 comport = ports[which_comport].device
 
-import serial
-ser = serial.Serial(comport, 9600)
-ser.write(b'001780')
-print(ser.readline())
 
+# Open the serial port
+ser = serial.Serial(comport, 9600)  # Make sure the baud rate matches your device
+
+# Send '001780' as ASCII encoded data
+data_to_send = '001780'
+ser.write(data_to_send.encode('ascii'))
+
+# Close the serial connection
+ser.close()
 
 
