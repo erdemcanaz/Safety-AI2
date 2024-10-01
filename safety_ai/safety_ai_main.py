808eec0e52e480918cbcba4804198fe9bfafad31
Erdem <erdemcanaz@gmail.com>
2024-10-01 21:58:04 +0300

    Update safety_ai_main.py

--- safety_ai/safety_ai_main.py
+++ safety_ai/safety_ai_main.py
@@ -91,12 +91,12 @@ while True:
         timestamp_str = datetime.datetime.now().strftime("%Y_%m_%d_%H")
 
         # update detected_people_count
-        api_dealer.update_count(camera_uuid= camera_uuid, subkey="detected_people_count", delta_count=number_of_people_detected)
-        api_dealer.update_count(camera_uuid= camera_uuid, subkey=f"detected_people_count_{timestamp_str}", delta_count=number_of_people_detected)
+        api_dealer.update_count(count_key= camera_uuid, count_subkey="detected_people_count", delta_count=number_of_people_detected)
+        api_dealer.update_count(count_key= camera_uuid, count_subkey=f"detected_people_count_{timestamp_str}", delta_count=number_of_people_detected)
 
         # update evaluated_frame_count
-        api_dealer.update_count(camera_uuid= camera_uuid, subkey="evaluated_frame_count", delta_count=1)
-        api_dealer.update_count(camera_uuid= camera_uuid, subkey=f"evaluated_frame_count_{timestamp_str}", delta_count=1)
+        api_dealer.update_count(count_key= camera_uuid, count_subkey="evaluated_frame_count", delta_count=1)
+        api_dealer.update_count(count_key= camera_uuid, count_subkey=f"evaluated_frame_count_{timestamp_str}", delta_count=1)
 
 
         for violation_report in evaluation_result['violation_reports']:
@@ -104,14 +104,14 @@ while True:
             violation_type = violation_report['violation_type']
             #    def update_count(self, count_key:str=None, count_subkey:str=None, delta_count:float = None):           
             if violation_type == "hardhat_detection":
-                api_dealer.update_count(camera_uuid= camera_uuid, subkey="detected_hardhat_count", delta_count=1)
-                api_dealer.update_count(camera_uuid= camera_uuid, subkey=f"detected_hardhat_count_{timestamp_str}", delta_count=1)
+                api_dealer.update_count(count_key= camera_uuid, count_subkey="detected_hardhat_count", delta_count=1)
+                api_dealer.update_count(count_key= camera_uuid, count_subkey=f"detected_hardhat_count_{timestamp_str}", delta_count=1)
             elif violation_type == "restricted_area_detection":
-                api_dealer.update_count(camera_uuid= camera_uuid, subkey="detected_restricted_area_count", delta_count=1)
-                api_dealer.update_count(camera_uuid= camera_uuid, subkey=f"detected_restricted_area_count_{timestamp_str}", delta_count=1)
+                api_dealer.update_count(count_key= camera_uuid, count_subkey="detected_restricted_area_count", delta_count=1)
+                api_dealer.update_count(count_key= camera_uuid, count_subkey=f"detected_restricted_area_count_{timestamp_str}", delta_count=1)
             elif violation_type == "check_person":
-                api_dealer.update_count(camera_uuid= camera_uuid, subkey="check_person_count", delta_count=1)
-                api_dealer.update_count(camera_uuid= camera_uuid, subkey=f"check_person_count_{timestamp_str}", delta_count=1)
+                api_dealer.update_count(count_key= camera_uuid, count_subkey="check_person_count", delta_count=1)
+                api_dealer.update_count(count_key= camera_uuid, count_subkey=f"check_person_count_{timestamp_str}", delta_count=1)
 
     continue
 
