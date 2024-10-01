2e4edb018cf5725cabe60a10b7be0164b9e6cc23
Erdem <erdemcanaz@gmail.com>
2024-10-01 23:03:44 +0300

    Update iot_device_module.py

--- safety_ai/modules/iot_device_module.py
+++ safety_ai/modules/iot_device_module.py
@@ -12,6 +12,10 @@ class IoTDevicemanager:
 
         self.last_time_signal_sent_to_iot_devices = {} #  key: iot_device_uuid | value: time.time() to prevent sending signals to iot devices too frequently
         pass
+    
+    def __send_signal_to_iot_device(self, iot_device_uuid:str, which_action:str):
+        #TODO:
+        print(f"Sending signal to iot_device_uuid: {iot_device_uuid} with action: {which_action}")
 
     def update_iot_devices(self, update_interval_seconds:float = 60):
         if time.time() - self.last_time_iot_devices_updated < update_interval_seconds: return
@@ -42,8 +46,26 @@ class IoTDevicemanager:
 
     def send_signal_to_iot_devices_if_rule_triggered_recently(self):
         response = self.api_dealer.fetch_all_rules()
-        pprint.pprint(response)
-        pass
+        rule_uuid_trigger_time_dict = {} # key: rule_uuid | 'YYYY-MM-DD HH:MM:SS' -> time.time()
+        if response[0] == True:
+            all_rules = response[2]
+            for rule in all_rules:
+                rule_uuid = rule['rule_uuid']
+                last_time_triggered = rule['last_time_triggered'] # 'YYYY-MM-DD HH:MM:SS'
+                rule_uuid_trigger_time_dict[rule_uuid] = time.mktime(time.strptime(last_time_triggered, "%Y-%m-%d %H:%M:%S"))
+        
+        for iot_device_uuid in self.iot_devices:
+            if iot_device_uuid not in self.last_time_signal_sent_to_iot_devices: self.last_time_signal_sent_to_iot_devices[iot_device_uuid] = 0
+            if time.time() - self.last_time_signal_sent_to_iot_devices[iot_device_uuid] < 20: continue
+            
+            for linked_rule_uuid_and_action in self.iot_devices[iot_device_uuid]['linked_rule_uuids_and_actions']:
+                rule_uuid = linked_rule_uuid_and_action[0]
+                which_action = linked_rule_uuid_and_action[1]
+                if rule_uuid in rule_uuid_trigger_time_dict and time.time() - rule_uuid_trigger_time_dict[rule_uuid] < 20:
+                    self.__send_signal_to_iot_device(iot_device_uuid, which_action)
+                    self.last_time_signal_sent_to_iot_devices[iot_device_uuid] = time.time()
+                
+                
 
         
         
