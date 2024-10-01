5f0910cc108da5a243a2254dc0b850960ff732be
Erdem <erdemcanaz@gmail.com>
2024-10-01 22:48:37 +0300

    Update iot_device_module.py

--- safety_ai/modules/iot_device_module.py
+++ safety_ai/modules/iot_device_module.py
@@ -7,7 +7,7 @@ class IoTDevicemanager:
 
     def __init__(self, api_dealer:safety_ai_api_dealer_module.SafetyAIApiDealer):
         self.api_dealer = api_dealer
-        self.iot_devices = {} # key: iot_device_uuid | device_name (str),  device_id (str), linked_rule_uuids (List[str])
+        self.iot_devices = {} # key: iot_device_uuid | device_uuid (str), device_name (str),  device_id (str), linked_rule_uuids (List[str])
         self.last_time_iot_devices_updated = 0
         pass
 
@@ -17,10 +17,12 @@ class IoTDevicemanager:
 
         response = self.api_dealer.fetch_all_iot_devices()
         if response[0] == True:
-            self.iot_devices = response[2]
+            self.iot_devices = {}    
+            for iot_device_dict in response[2]:
+                self.iot_devices[iot_device_dict['device_uuid']] = iot_device_dict
             pprint.pprint(self.iot_devices)
         else:
-            print(f"Error: {response[1]}")
+            print("Error: ", response[1])
         
 
 
