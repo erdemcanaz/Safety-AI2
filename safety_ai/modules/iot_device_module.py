ccb56139cd92684f6867d80f9b907761e5175826
Erdem <erdemcanaz@gmail.com>
2024-10-03 11:32:41 +0300

    Update iot_device_module.py

--- safety_ai/modules/iot_device_module.py
+++ safety_ai/modules/iot_device_module.py
@@ -19,7 +19,7 @@ class IoTDevicemanager:
     
     def ensure_serial_port_is_open(self):
         # Check if the serial port was previously opened
-        print(self.serial_port, self.serial_port.is_open)
+        print(self.serial_port)
         if self.serial_port is not None and self.serial_port.is_open:
             # Get the list of current COM ports
             current_ports = [port.device for port in serial.tools.list_ports.comports()]
