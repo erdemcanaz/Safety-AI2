a2cde498af8fb67676a108dd924b35b12ba29904
Erdem <erdemcanaz@gmail.com>
2025-01-21 16:56:13 +0300

    Update frame_evaluator_module.py

--- safety_ai/modules/frame_evaluator_module.py
+++ safety_ai/modules/frame_evaluator_module.py
@@ -149,6 +149,7 @@ class FrameEvaluator():
                 picasso_module.draw_image_on_frame(frame=evaluation_result['processed_cv2_frame'], image_name="red_hardhat_transparent_with_background", x=bbox[2]+padding, y=bbox[3]-4*padding, width=icon_max_size, height=icon_max_size, maintain_aspect_ratio=True)
 
         if len(evaluation_result['violation_reports']) > 0:
+            print("zaa")
             resized_frame = cv2.resize(copy.deepcopy(evaluation_result['processed_cv2_frame']), (500, 500))
             if PREFERENCES.SHOW_FRAMES['combined_violation_frame']: cv2.imshow("Combined violation frame", resized_frame)
 
